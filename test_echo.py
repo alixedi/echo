4ded8fc70ecbd8ce286c12e12e178f75d16edc84
Jim Witschey <jim.witschey@gmail.com>
2014-10-25 18:47:21 -0400

    adds tests for exact number of failures

--- test_echo.py
+++ test_echo.py
@@ -2,6 +2,18 @@ import unittest
 from pyecho import echo, FailingTooHard
 
 
+def fails_n_times(n, exception_type=Exception):
+    # remembers how many times it's called, fails n times, returns None after
+    _called = 0
+    def fail_func():
+        nonlocal _called
+        if _called >= n:
+            return
+        _called += 1
+        raise exception_type()
+    return fail_func
+
+
 class TestEcho(unittest.TestCase):
 
     def test_without_failing(self):
@@ -15,6 +27,18 @@ class TestEcho(unittest.TestCase):
         def conct():
             return 2 + "two"
         self.assertRaises(FailingTooHard, conct)
+
+    def test_n_minus_one_tries(self):
+        try:
+            # function fails twice; try 3 times
+            echo(3)(fails_n_times(2))()
+        except FailingTooHard:
+            # test fails if wrapped function doesn't succeed
+            self.fail()
+
+    def test_doesnt_try_too_many_times(self):
+        wrapped = echo(2)(fails_n_times(2))
+        self.assertRaises(FailingTooHard, wrapped)
 
 
 if __name__ == "__main__":
