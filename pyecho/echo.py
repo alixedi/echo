572d6167391c481c614afc105a11cd8fdaa642db
Nauman Ahmad <nauman-ahmad@outlook.com>
2014-10-26 11:18:53 +0500

    Version 0.0.2 :+1:

--- pyecho/echo.py
+++ pyecho/echo.py
@@ -7,7 +7,7 @@
 
 author: Nauman Ahmad
 description: A micro library for retrying failing operations.
-version: 0.0.1
+version: 0.0.2
 license: MIT
 """
 from functools import wraps
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyecho',
-      version='0.0.1',
+      version='0.0.2',
       description='A micro library for retrying failing operations.',
       url='http://github.com/itsnauman/echo',
       author='Nauman Ahmad',
